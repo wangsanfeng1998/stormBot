bb6fe2ca34d04659d408088e811973468c1f064e
Sanfeng Wang <swang@fulcrum-gt.com>
2017-07-28 10:57:09 -0500

    Infinite Loop
    Made it so calls are constantly being made while running the script

--- auto_reply.py
+++ auto_reply.py
@@ -1,19 +1,39 @@
 import praw
-import pdb
-import re
-import os
+import config
+#import pdb
+#import re
+#import os
+
 #from praw.helpers import comment_stream
- 
-reddit = praw.Reddit("stormBot")
-#r.login()
- 
-target_text = "stormlight"
-response_text = "I love Stormlight! Great to see a fellow fan"
- 
-processed = []
+def bot_login():
+	r = praw.Reddit(username = config.username,
+				password = config.password,
+				client_id = config.client_id,
+				client_secret = config.client_secret,
+				user_agent = "Mist_Bot v 0.1")
+	return r
+
+def run_bot(r):
+	processed = []
+	for comment in r.subreddit("fantasy").comments(limit = 25):
+		if "mistborn" in comment.body and comment.id not in processed:
+			comment.reply("Mistborn is great! Hope to see a video game soon")
+			print ("Responded to comment" + comment.id)
+			processed.append(comment.id)
+
+r = bot_login()
+
 while True:
-    for c in reddit.subreddit('fantasy').stream.comments():
-        if target_text in c.body and c.id not in processed:
-            c.reply(response_text)
-            print('responded to ' + target_text)
-            processed.append(c.id)
+	run_bot(r)
+
+ 
+#target_text = "stormlight"
+#response_text = "I love Stormlight! Great to see a fellow fan"
+ 
+#processed = []
+#while True:
+#    for c in reddit.subreddit('fantasy').stream.comments():
+#        if target_text in c.body and c.id not in processed:
+#            c.reply(response_text)
+#            print('responded to ' + target_text)
+#            processed.append(c.id)
---
+++ config.py
@@ -0,0 +1,4 @@
+username = "Mist_Bot"
+password = "mistborn_rocks"
+client_id = "RGgxvm3w569Stw"
+client_secret = "XuTzJ9vCY5YL8YhJuVmn2eOH7DA"
